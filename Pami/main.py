c32e29e05742381311b737e1ad381949db7efac1
TechnoSpiritMC <127853301+TechnoSpiritMC@users.noreply.github.com>
2024-11-05 20:59:40 +0100

    Update main.py

--- Pami/main.py
+++ Pami/main.py
@@ -1,15 +1,71 @@
+import time
+
+
+def LDA(start, angle, deviation):
+    """
+    :param start: ``int`` → The time when the measurements were taken.
+    :param angle: ``int`` → The last measured angle.
+    :param deviation: ``int`` → The deviation in degrees / seconds
+    """
+
+    currentDeviation = (time.time() - start) * deviation
+
+    print("Current Deviation:", currentDeviation)
+
+    currentAngle = angle - currentDeviation
+    return currentAngle
+
+
+def RDA(lastAngles):
+    """
+    :param lastAngles: ``list[int]`` → The last measured angles (The more, the better).
+    """
+
+    averageAngle = sum(lastAngles) / len(lastAngles)
+    return averageAngle
+
+
+class Buffer:
+    def __init__(self, array):
+        self.array = array
+
+    @staticmethod
+    def InitiateBuffer(length):
+        """
+        :param length: ``int`` → The length of the desired buffer.
+        """
+
+        return [0 for i in range(length)]
+
+    def Slip(self, value):
+        """
+        :param value: ``any`` → Something to add to that buffer.
+        """
+
+        for i in range(len(self.array) - 1):
+            self.array[i] = self.array[i + 1]
+        self.array[0] = None
+        self.array[-1] = value
+
+        return self.array
+
+    def getAverageValue(self):
+        return sum(self.array) / len(self.array)
+
+    def getMinValue(self):
+        return min(self.array[0])
+
+    def getMaxValue(self):
+        return max(self.array[0])
+
+
 def AreSimilar(List, Tolerance):
-
-    print(List)
-
     Average = sum(List) / len(List)
     Similar = True
 
     for item in List:
 
         difference = abs(Average - item)
-        print(difference)
-
         if difference < Tolerance:
             pass
         else:
@@ -19,26 +75,37 @@ def AreSimilar(List, Tolerance):
     return Similar
 
 
-def DefineAlogorythm(Values):
+def DefineAlgorithm(Values):
     BASETOLERANCE = 5
     d = []
 
-    for _ in range(len(Values)-1):
+    for _ in range(len(Values) - 1):
         i = (len(Values) - 1) - _
-
-        print("i", Values[i], "i-1", Values[i-1])
-
-        d.append(Values[i] - Values[i-1])
+        d.append(Values[i] - Values[i - 1])
 
     Average = sum(d) / len(d)
-    Tolerance = BASETOLERANCE + abs(Average/BASETOLERANCE)
+    Tolerance = BASETOLERANCE + abs(Average / BASETOLERANCE)
+
     if AreSimilar(d, Tolerance):
-        print(__name__, " Using linear drift algorythm.")
+        return "LDA", Average
 
     else:
-        print(__name__, "Using random drift algorythm.")
+        return "RDA", None
 
 
 if __name__ == "__main__":
-    values = [10, 20, 30, 40, 50]
-    DefineAlogorythm(values)
+    startTime = time.time()
+    values = [i*10 for i in range(10000)]
+    Algorithm, Opt = DefineAlgorithm(values)
+
+    print(Algorithm, Opt)
+
+    if Algorithm == "LDA":
+        print(__name__, " Using linear drift algorythm.")
+        Average = Opt
+
+        for i in range(len(values)):
+            print(LDA(startTime, values[i], Average))
+
+    elif Algorithm == "RDA":
+        print(__name__, " Using random drift algorythm.")
